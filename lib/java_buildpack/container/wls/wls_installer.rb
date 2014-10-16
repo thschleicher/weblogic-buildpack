10574a4b7e553107c21fdec1d54e41414df5ac5b
thschleicher <th@schleicher-diez.de>
2014-10-16 16:02:24 +0200

    Changed install_post_args

--- lib/java_buildpack/container/wls/wls_installer.rb
+++ lib/java_buildpack/container/wls/wls_installer.rb
@@ -202,7 +202,8 @@ module JavaBuildpack
           install_pre_args << " ln -s #{install_binary_file} #{new_binary_path}; "
           install_pre_args << " mkdir #{@wls_install_path}; chmod +x #{new_binary_path}; "
 
-          install_post_args = " -silent -responseFile #{wls_install_response_file_target}"
+          # install_post_args = " -silent -responseFile #{wls_install_response_file_target}"
+		      install_post_args = " -mode=silent -silent_xml=#{wls_install_response_file_target}"
           install_post_args << " -invPtrLoc #{ora_install_inventory_target}"
 
           install_command = install_pre_args + install_command_args +  install_post_args
